0e1dfaa4cc5bae1902d48c6d8ec0b8d09ac34be2
Łukasz <lkowalik04@gmail.com>
2024-03-19 10:35:22 +0100

    lint

--- task1/2.c
+++ task1/2.c
@@ -5,29 +5,29 @@
 #include <unistd.h>
 
 int main() {
-    for (int i = 0; i < 3; i++) {
-        printf("Iteracja numer: %d\n", i + 1);
-        pid_t pid = fork();
+  for (int i = 0; i < 3; i++) {
+    printf("Iteracja numer: %d\n", i + 1);
+    pid_t pid = fork();
 
-        switch (pid) {
-            case -1:
-                perror("fork");
-                exit(EXIT_FAILURE);
-                break;
-            case 0:
-                printf("UID: %d\n", getuid());
-                printf("GID: %d\n", getgid());
-                printf("PID: %d\n", getpid());
-                printf("PPID: %d\n", getppid());
-                printf("PGID: %d\n", getpgid(0));
-                exit(EXIT_SUCCESS);
-                break;
-            default:
-                int status;
-                wait(&status);
-                break;
-        }
+    switch (pid) {
+      case -1:
+        perror("fork");
+        exit(EXIT_FAILURE);
+        break;
+      case 0:
+        printf("UID: %d\n", getuid());
+        printf("GID: %d\n", getgid());
+        printf("PID: %d\n", getpid());
+        printf("PPID: %d\n", getppid());
+        printf("PGID: %d\n", getpgid(0));
+        exit(EXIT_SUCCESS);
+        break;
+      default:
+        int status;
+        wait(&status);
+        break;
     }
+  }
 
-    return 0;
+  return 0;
 }
