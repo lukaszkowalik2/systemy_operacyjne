46053ee30869d0651b5c904b13123ca55d9edfb8
Lukasz <lkowalik04@gmail.com>
2024-04-03 09:33:49 +0200

    fix for task 2

--- task2/1.c
+++ task2/1.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+  int err = 0;
   for (int i = 0; i < 3; i++) {
     printf("Iteracja numer: %d\n", i + 1);
     pid_t pid = fork();
@@ -15,9 +16,11 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
         break;
       case 0:
-        execlp(argv[1], argv[1], (char *)NULL);
-        perror("execlp error");
-        _exit(2);
+        err = execlp(argv[1], argv[1], (char *)NULL);
+        if (err == -1) {
+          perror("execlp error");
+          _exit(2);
+        }
         break;
       default:
         wait(NULL);
