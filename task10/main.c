36cfb36dee0a8ac74343f720bf051ca15e217e30
Lukasz <lkowalik04@gmail.com>
2024-06-09 23:03:34 +0200

    task 10

--- task10/main.c
+++ task10/main.c
@@ -1,5 +1,8 @@
-#include <stdlib.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define THREAD_COUNT 5
 #define ITERATIONS 3
@@ -9,62 +12,73 @@
 volatile int tickets[THREAD_COUNT];
 volatile int choosing[THREAD_COUNT];
 
-int counter = 0;
+int shared_counter = 0;
 
 void gotoxy(unsigned x, unsigned y) {
   printf("\033[%d;%dH\033[2K", y, x);
 }
 
-int max_number() {
-  int max = 0;
+int get_max_ticket() {
+  int max_ticket = 0;
+
   for (int i = 0; i < THREAD_COUNT; i++) {
-    if (tickets[i] > max) {
-      max = tickets[i];
+    if (tickets[i] > max_ticket) {
+      max_ticket = tickets[i];
     }
   }
-  return max;
+
+  return max_ticket;
 }
 
 void* thread_function(void* arg) {
-  int thread = *((int*)arg) ;
+  int thread_id = *((int*)arg);
   for (int i = 0; i < ITERATIONS; i++) {
-    gotoxy(1, thread + 1);
-    printf("Wątek %d: sekcja prywatna\n", thread +1);
+    gotoxy(1, thread_id + 1);
+    printf("Wątek %d: sekcja prywatna\n", thread_id + 1);
     sleep(1);
-    
-    choosing[thread] = 1;
-    tickets[thread] = max_number() + 1;
-    choosing[thread] = 0;
 
-    for (int other = 0; other < THREAD_COUNT; ++other) {
-      while (choosing[other]);
-      while (tickets[other] != 0 && (tickets[other] < tickets[index] || (tickets[other] == tickets[index] && other < index)));
+    choosing[thread_id] = 1;
+    tickets[thread_id] = get_max_ticket() + 1;
+    choosing[thread_id] = 0;
+
+    for (int other_thread = 0; other_thread < THREAD_COUNT; ++other_thread) {
+      while (choosing[other_thread]);
+      while (tickets[other_thread] != 0 &&
+             (tickets[other_thread] < tickets[thread_id] ||
+              (tickets[other_thread] == tickets[thread_id] && other_thread < thread_id)));
     }
 
-    gotoxy(XMAX, thread + 1);
-    printf("Wątek %d: sekcja krytyczna\n", thread + 1);
+    gotoxy(XMAX, thread_id + 1);
+    printf("Wątek %d: sekcja krytyczna\n", thread_id + 1);
 
-    counter++;
+    shared_counter++;
     sleep(1);
-    tickets[thread] = 0;
+    tickets[thread_id] = 0;
 
-    gotoxy(1, thread + 1);
-    printf("Wątek %d: sekcja prywatna\n", thread + 1);
+    gotoxy(1, thread_id + 1);
+    printf("Wątek %d: sekcja prywatna\n", thread_id + 1);
 
     sleep(1);
   }
-  return NULL;
+
+  pthread_exit(NULL);
 }
 
 int main(int argc, char** argv) {
   pthread_t threads[THREAD_COUNT];
-  int thread_ids[THREAD_COUNT];  
-  system("clear");
+  int thread_ids[THREAD_COUNT];
+
+  int system_result = system("clear");
+  if (system_result == -1) {
+    perror("systm(\"clear\")");
+    exit(EXIT_FAILURE);
+  }
+
   for (int i = 0; i < THREAD_COUNT; ++i) {
     thread_ids[i] = i;
     int create_result = pthread_create(&threads[i], NULL, thread_function, (void*)&thread_ids[i]);
     if (create_result != 0) {
-      fprintf(stderr, "Nie udało się utworzyć wątku: %s\n", strerror(create_result));
+      fprintf(stderr, "Nie udało się utworzyć wątku %d: %s\n", i, strerror(create_result));
       exit(EXIT_FAILURE);
     }
   }
@@ -72,25 +86,16 @@ int main(int argc, char** argv) {
   for (int i = 0; i < THREAD_COUNT; ++i) {
     int join_result = pthread_join(threads[i], NULL);
     if (join_result != 0) {
-      fprintf(stderr, "Nie udało się dołączyć wątku: %s\n", strerror(join_result));
+      fprintf(stderr, "Nie udało się dołączyć wątku %d: %s\n", i, strerror(join_result));
       exit(EXIT_FAILURE);
-    }  
+    }
   }
 
-  printf("\nWartość wspólnego licznika: %d\n", counter);
-  if (counter == THREAD_COUNT * ITERATIONS) {
+  printf("\nWartość wspólnego licznika: %d\n", shared_counter);
+  if (shared_counter == THREAD_COUNT * ITERATIONS) {
     printf("Wartość jest poprawna.\n");
   } else {
     printf("Wartość jest niepoprawna.\n");
-  }
-
-  for (int i = 0; i < THREAD_COUNT; ++i) {
-    thread_ids[i] = i;
-    int exit_result = pthread_exit(threads[i]);
-    if (exit_result != 0) {
-      fprintf(stderr, "Nie udało się usunąć wątku: %s\n", strerror(exit_result));
-      exit(EXIT_FAILURE);
-    }
   }
 
   return 0;
